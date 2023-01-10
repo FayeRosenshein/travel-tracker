f208a5513f4abde395de64680122474d49281907
Faye Rosenshein <rosefaye.a@gmail.com>
2023-01-10 15:58:23 -0700

    feat: Add an upcoming trip page

--- src/scripts.js
+++ src/scripts.js
@@ -9,3 +9,13 @@ import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+// id: 167,
+// userID: 15,
+// destinationID: 42,
+// travelers: 3,
+// date: "2019/08/10",
+// duration: 16,
+// status: "approved",
+// suggestedActivities: [ ]
